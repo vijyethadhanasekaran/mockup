332887a2cd6e468873a9199eb6bb4bde6a54ae2d
Vijyetha <101820774+vijyethadhanasekaran@users.noreply.github.com>
2023-04-10 15:15:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,15 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Grid, Card, CardContent, CardMedia, CardActions, Button } from '@material-ui/core';
+import FilterSection from './components/FilterSection'; // Import the FilterSection component
 
-const App = () => {
-  // Static data for cards
-  const cards = [
-    { title: 'Card Title 1', description: 'Card description 1', imageUrl: 'https://via.placeholder.com/150' },
-    { title: 'Card Title 2', description: 'Card description 2', imageUrl: 'https://via.placeholder.com/150' },
-    { title: 'Card Title 3', description: 'Card description 3', imageUrl: 'https://via.placeholder.com/150' },
-  ];
-
+function App() {
   return (
-    <div>
-      {/* Header */}
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">Header</Typography>
-        </Toolbar>
-      </AppBar>
-
-      {/* Card Grid */}
-      <Grid container spacing={2} style={{ padding: 16 }}>
-        {cards.map((card, index) => (
-          <Grid item xs={12} sm={4} key={index}>
-            <Card>
-              <CardMedia
-                component="img"
-                alt={card.title}
-                height="140"
-                image={card.imageUrl}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {card.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {card.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Action 1</Button>
-                <Button size="small">Action 2</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-
-      {/* Footer */}
-      <AppBar position="static" style={{ marginTop: 16 }}>
-        <Toolbar>
-          <Typography variant="h6">Footer</Typography>
-        </Toolbar>
-      </AppBar>
+    <div className="App">
+      <h1>My React Application</h1>
+      {/* Include the FilterSection component */}
+      <FilterSection />
     </div>
   );
-};
+}
 
 export default App;
 
